5aff6710b81fd5d02ebf66826a477fbd7353266f
Julian Rojas <julianrojasgeo@gmail.com>
2023-03-22 10:56:16 -0500

    get all and get by id tests

--- src/test/java/com/reactiveAPP/coursesAPI/usecases/GetAllCoursesUseCaseTest.java
+++ src/test/java/com/reactiveAPP/coursesAPI/usecases/GetAllCoursesUseCaseTest.java
@@ -1,10 +1,17 @@
 package com.reactiveAPP.coursesAPI.usecases;
 
 import com.reactiveAPP.coursesAPI.repository.CourseRepository;
+import com.reactiveAPP.coursesAPI.util.InstanceProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +22,29 @@ class GetAllCoursesUseCaseTest {
     CourseRepository repoMock;
     ModelMapper mapper;
     GetAllCoursesUseCase getAllCoursesUseCase;
+
+    @BeforeEach
+    void init(){
+        mapper = new ModelMapper();
+        getAllCoursesUseCase = new GetAllCoursesUseCase(repoMock, mapper);
+    }
+
+    @Test
+    @DisplayName("getAllCourses_Success")
+    void getAllBooks(){
+        var fluxStudents = Flux.just(
+                InstanceProvider.getCourses().get(0),
+                InstanceProvider.getCourses().get(1),
+                InstanceProvider.getCourses().get(2));
+
+        Mockito.when(repoMock.findAll()).thenReturn(fluxStudents);
+
+        var service = getAllCoursesUseCase.get();
+
+        StepVerifier.create(service)
+                .expectNextCount(3)
+                .verifyComplete();
+        Mockito.verify(repoMock).findAll();
+    }
 
 }
--- src/test/java/com/reactiveAPP/coursesAPI/usecases/GetCourseByIdUseCaseTest.java
+++ src/test/java/com/reactiveAPP/coursesAPI/usecases/GetCourseByIdUseCaseTest.java
@@ -1,10 +1,18 @@
 package com.reactiveAPP.coursesAPI.usecases;
 
+import com.reactiveAPP.coursesAPI.domain.dto.CourseDTO;
 import com.reactiveAPP.coursesAPI.repository.CourseRepository;
+import com.reactiveAPP.coursesAPI.util.InstanceProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +22,43 @@ class GetCourseByIdUseCaseTest {
     @Mock
     CourseRepository repoMock;
     ModelMapper mapper;
-    GetCourseByIdUseCase courseByIdUseCase;
+    GetCourseByIdUseCase getCourseByIdUseCase;
 
+    @BeforeEach
+    void init(){
+        mapper = new ModelMapper();
+        getCourseByIdUseCase = new GetCourseByIdUseCase(repoMock, mapper);
+    }
+
+    @Test
+    @DisplayName("getCourseById_Success")
+    void getStudentById(){
+        var courseID = "ID1";
+        var course = Mono.just(InstanceProvider.getCourse());
+
+        Mockito.when(repoMock.findById(courseID)).thenReturn(course);
+
+        var service = getCourseByIdUseCase.apply(courseID);
+
+        StepVerifier.create(service)
+                //.expectNextCount(1)
+                .expectNext(mapper.map(InstanceProvider.getCourses().get(2), CourseDTO.class))
+                .verifyComplete();
+        Mockito.verify(repoMock).findById(courseID);
+    }
+
+    @Test
+    @DisplayName("getStudentById_NonSuccess")
+    void getStudentByNonExistingID(){
+        var courseID = "ID1";
+
+        Mockito.when(repoMock.findById(courseID)).thenReturn(Mono.empty());
+
+        var service = getCourseByIdUseCase.apply(courseID);
+
+        StepVerifier.create(service)
+                .expectNextCount(0)
+                .verifyComplete();
+        Mockito.verify(repoMock).findById(courseID);
+    }
 }
