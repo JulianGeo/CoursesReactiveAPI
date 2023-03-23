62ae4379c698007d1247f837d1194ccaac55a7e7
Julian Rojas <julianrojasgeo@gmail.com>
2023-03-23 15:15:49 -0500

    Unenroll consumer use case implemented with stringID

--- src/main/java/com/reactiveAPP/coursesAPI/consumer/CourseConsumer.java
+++ src/main/java/com/reactiveAPP/coursesAPI/consumer/CourseConsumer.java
@@ -3,6 +3,7 @@ package com.reactiveAPP.coursesAPI.consumer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.reactiveAPP.coursesAPI.usecases.EnrollStudentUseCase;
+import com.reactiveAPP.coursesAPI.usecases.UnenrollCourseUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -12,12 +13,17 @@ import org.springframework.stereotype.Component;
 public class CourseConsumer {
     private final ObjectMapper objectMapper;
     private final EnrollStudentUseCase enrollStudentUseCase;
+    private final UnenrollCourseUseCase unenrollCourseUseCase;
 
     @RabbitListener(queues = "students.queue")
     public void receiveEventBook(String message) throws JsonProcessingException {
         StudentEvent event = objectMapper.readValue(message, StudentEvent.class);
-        System.out.println("student id"+event.getStudentID());
-        enrollStudentUseCase.apply(event.getStudentID(), event.getCourseID()).subscribe();
+        if (event.getEventType().equals("enroll")){
+            enrollStudentUseCase.apply(event.getStudentID(), event.getCourseID()).subscribe();
+        }
+        if (event.getEventType().equals("unenroll")){
+            unenrollCourseUseCase.apply(event.getStudentID(), event.getCourseID()).subscribe();
+        }
     }
 
 }
--- src/main/java/com/reactiveAPP/coursesAPI/usecases/UnenrollCourseUseCase.java
+++ src/main/java/com/reactiveAPP/coursesAPI/usecases/UnenrollCourseUseCase.java
@@ -1,4 +1,37 @@
 package com.reactiveAPP.coursesAPI.usecases;
 
-public class UnenrollCourseUseCase {
+import com.reactiveAPP.coursesAPI.domain.dto.CourseDTO;
+import com.reactiveAPP.coursesAPI.repository.CourseRepository;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+import java.util.function.BiFunction;
+
+@Service
+@AllArgsConstructor
+public class UnenrollCourseUseCase implements BiFunction<String, String, Mono<CourseDTO>> {
+
+    private final ModelMapper mapper;
+    private final CourseRepository courseRepository;
+
+    @Override
+    public Mono<CourseDTO> apply(String studentID, String courseID) {
+/*        System.out.println("use case used");
+        System.out.println("courseID:" +courseID);*/
+        return this.courseRepository
+                .findById(courseID)
+                .switchIfEmpty(Mono.error(new Throwable("Course not found")))
+                .flatMap(course -> {
+                    Set<String> students1 = course.getStudentDTOSID();
+                    if (!students1.contains(studentID)){
+                        return Mono.error(new Throwable("Student not enrolled yet"))     ;
+                    }
+                    students1.remove(studentID);
+                    course.setStudentDTOSID(students1);
+                    return this.courseRepository.save(course);
+                }).map(course -> mapper.map(course, CourseDTO.class));
+    }
 }
